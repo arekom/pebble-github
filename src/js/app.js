8100fc6e1b327254cbf87b0bb269a4fc9a3c0dda
arekom <ar1s336@gmail.com>
2015-07-21 15:25:55 +0100

    push

--- src/js/app.js
+++ src/js/app.js
@@ -1,60 +1,47 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var UI = require('ui');
-var Vector2 = require('vector2');
-
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+var ajax = require('ajax');
+var splashCard = new UI.Card({
+    title: 'Please Wait',
+    body: 'Loading request...',
+    bodyColor: 'tiffanyBlue',
+    titleColor: 'orange',
+    font: 'GOTHIC_14'
 });
+splashCard.show();
 
-main.show();
+var URL = 'https://api.github.com/users/arekom/repos';
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
+ajax({
+    url: URL,
+    type: 'json'
+}, function(json) {
+    var resultList = new UI.Menu({
+        font: 'GOTHIC_14',
+        sections: [{
+            title: 'Repositories',
+            items: [{
+                title: json[0].name,
+                subtitle: json[0].description
+            }, {
+                title: json[1].name,
+                subtitle: json[1].description
+            }, {
+                title: json[2].name,
+                subtitle: json[3].description
+            }]
+        }]
+    });
+    resultList.show();
+    splashCard.hide();
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
+    resultList.on('select', function(e) {
+        var details = new UI.Card({
+            title: 'Details for:',
+            body: json[e.itemIndex].name
+        });
+        details.show();
+    })
 
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
+}, function(error) {
+    console.log('Loading failed: ' + error);
 });
